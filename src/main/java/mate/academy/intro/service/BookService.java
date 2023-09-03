031e0941d1b1caec7284502946f0e78822c298e0
Wasja Zejkan <vasazejkan5@gmail.com>
2023-09-03 23:42:49 +0300

    Added Liquibase support, soft delete concept, replaced own BookRepository implementation with Spring Data Jpa Repository, added ability to update and delete Book entities

--- src/main/java/mate/academy/intro/service/BookService.java
+++ src/main/java/mate/academy/intro/service/BookService.java
@@ -10,4 +10,8 @@ public interface BookService {
     List<BookDto> findAll();
 
     BookDto getById(Long id);
+
+    BookDto updateById(Long id, CreateBookRequestDto bookDto);
+
+    void deleteById(Long id);
 }
